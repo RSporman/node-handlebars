545930ab5c3bdb627310753d1da83abf5940e9cd
Bobby Sporman <bobby.sporman@yahoo.com>
2020-11-16 18:38:26 -0500

    connecting databse

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('Bigger-Mac');
+INSERT INTO burgers (name) VALUES ('Chester-Burger');
+INSERT INTO burgers (name) VALUES ('Mary-Burger');
+INSERT INTO burgers (name) VALUES ('Fully-Loaded-Burger');
+INSERT INTO burgers (name) VALUES ('Whoppa');
